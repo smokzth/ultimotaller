1b624a60fac7ba226141d1c7b5bab52973a0f78a
Juan Pablo Gaviria Franco <francopablo155@gmail.com>
2025-05-12 19:05:50 -0500

    se añade la clase reglavalidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -11,3 +11,25 @@ from validadorclave.modelo.errores import (
 )
 
 
+class ReglaValidacion(ABC):
+
+    def __init__(self, longitud_esperada):
+        self._longitud_esperada = longitud_esperada
+
+    def _validar_longitud(self, clave):
+        return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave):
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave):
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave):
+        return any(c.isdigit() for c in clave)
+
+    @abstractmethod
+    def es_valida(self, clave):
+        pass
+
+
